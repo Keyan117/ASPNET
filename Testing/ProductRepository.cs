3807d82f9155db9afa83ee56cf2c0b5e07d2d879
Keyan117 <kdaneshvari98@gmail.com>
2023-04-20 13:08:17 -0700

    update

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -29,4 +29,10 @@ namespace Testing
         }
 
     }
+
+    public void UpdateProduct(Product product)
+    {
+         _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+     new {name = product.Name, price = product.Price, id = product.ProductID });
+    }
 }
