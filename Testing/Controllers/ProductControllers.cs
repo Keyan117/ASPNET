6bd56bb8352c6c0c23ce562cf5b07e32eb910208
Keyan117 <kdaneshvari98@gmail.com>
2023-04-24 21:43:58 -0700

    updated101

--- Testing/Controllers/ProductControllers.cs
+++ Testing/Controllers/ProductControllers.cs
@@ -65,5 +65,15 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult DeleteProduct(Product product)
+        {
+        
+            repo.DeleteProduct(product);
+        
+            return RedirectToAction("Index");
+        
+        }
+
+
     }
 }
